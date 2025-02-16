4677d184dd28afaa79b1eb8f4cd3907c0af50c27
Damien Gallagher <damien.gallagher@gmail.com>
2025-02-16 23:43:42 +0000

    Apollo POC

--- AnyCompanyReads-frontend/README.md
+++
@@ -1,1 +0,0 @@
-## AnyCompany Reads frontend application
---
+++ fs-apollo/src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql } from '@apollo/client';
+import logo from './logo.svg';
+import config from './aws-exports';
+
+const { endpoint } = config.aws_cloud_logic_custom[0];
+console.log({ endpoint });
+// Query definition
+const query = gql`
+  query {
+    hello
+  }
+`;
+
+function App() {
+  const { loading, error, data } = useQuery(query);
+
+  if (loading) return <h3>Loading...</h3>;
+  if (error) {
+    console.log({ error });
+    return <h3>Error occurred: {error.message}</h3>;
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>{data.hello}</h1>
+      </header>
+    </div>
+  );
+}
+
+// Create Apollo Client instance
+const client = new ApolloClient({
+  uri: endpoint + '/graphql',
+  cache: new InMemoryCache(), // Use InMemoryCache for caching queries
+});
+
+const AppWithProvider = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
+export default AppWithProvider;
